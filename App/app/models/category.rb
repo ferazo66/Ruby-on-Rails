13a8445453c66baa7a79a1c4379778ac6edcc991
ferazo66 <en-rike66@hotmail.com>
2017-03-09 12:21:29 -0500

    funcional estados completo

--- App/app/models/category.rb
+++
@@ -1,5 +0,0 @@
-class Category < ApplicationRecord
-  validates :name, presence: true
-  has_many :has_categories
-  has_many :articles, through: :has_categories
-end
--- App/app/models/has_category.rb
+++
@@ -1,4 +0,0 @@
-class HasCategory < ApplicationRecord
-  belongs_to :article
-  belongs_to :category
-end
