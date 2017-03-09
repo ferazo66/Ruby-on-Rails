bad7cd18ed3096345910495a6200071d521bff22
ferazo66 <en-rike66@hotmail.com>
2017-03-09 14:31:02 -0500

    funcional maquina de estados

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_article, except: [:index,:new,:create]
+  before_action :authenticate_editor!,only: [:new,:create,:edit]
+  before_action :authenticate_admin!,only: [:destroy]
+  def index
+    @article=Article.all
+  end
+  def show
+    @article.update_visits_count
+    @comment=Comment.new
+  end
+  def new
+    @article=Article.new
+    @categories =Category.all
+  end
+  def create
+    @article=current_user.articles.new(article_params)
+    @article.categories =params[:categories]
+   if @article.save
+    redirect_to @article
+   else
+     render :new
+   end
+  end
+  def edit
+  end
+  def update
+    if@article.update(article_params)
+      redirect_to @article
+    else
+      render :'articles/edit'
+    end
+  end
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title,:body,:cover,:categories)
+  end
+  def set_article
+    @article=Article.find(params[:id])
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :articles
+  has_many :comments
+  has_many :cover
+
+  def is_normal_user?
+    self.permission_level >=1
+  end
+  def is_editor?
+    self.permission_level >=2
+  end
+  def is_admin?
+    self.permission_level >=3
+  end
+end
